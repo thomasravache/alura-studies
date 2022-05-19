522d3ae65f7b974c52d922717bdb9f2ac0c75c38
Thomas Ravache <thomasravache31@gmail.com>
2022-05-19 12:49:02 -0300

    feat: renderizar array com map

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 
 function Lista() {
+  const tarefas = [
+    {
+      tarefa: 'React',
+      tempo: '02:00:00',
+    },
+    {
+      tarefa: 'Javascript',
+      tempo: '01:00:00',
+    }
+  ];
+
   return(
     <aside>
       <h2>Estudos do dia</h2>
       <ul>
-        <li>
-          <h3>React</h3>
-          <span>
-            02:00:00
-          </span>
-        </li>
-        <li>
-          <h3>
-            Javacript
-          </h3>
-          <span>
-            01:00:00
-          </span>
-        </li>
+        {tarefas.map((tarefa, index) => (
+          <li key={index}>
+            <h3>{tarefa.tarefa}</h3>
+            <span>{tarefa.tempo}</span>
+          </li>
+        ))}
       </ul>
     </aside>
   );
