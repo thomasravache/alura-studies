ee631fbf49e2fe2247347d57aa8f8f23375eeaa8
Thomas Ravache <thomasravache31@gmail.com>
2022-05-19 17:46:47 -0300

    feat: Utilizar props com tipagem 1

---
+++ src/components/Lista/Item/index.tsx
@@ -0,0 +1,11 @@
+function Item(props: { tarefa: string, tempo: string }) {
+  const { tarefa, tempo } = props;
+  return(
+    <li className={style.item}>
+      <h3>{tarefa}</h3>
+      <span>{tempo}</span>
+    </li>
+  );
+}
+
+export default Item;
