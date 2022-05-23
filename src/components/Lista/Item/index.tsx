f90e67dc9a96ececbeef61c5d6c21361e62562c9
Thomas Ravache <thomasravache31@gmail.com>
2022-05-23 08:25:52 -0300

    feat: criar funcao de conversao de tempo para segundos

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -2,7 +2,6 @@ import { IItemProps } from '../../../types/interfaces';
 import style from './Item.module.scss';
 
 function Item({ tarefa, tempo, selecionado, completado, id , selecionaTarefa }: IItemProps) {
-  console.log('item atual', { tarefa, tempo, selecionado, completado, id });
   return(
     <li
       className={`${style.item} ${selecionado ? style.itemSelecionado: ''}`}
